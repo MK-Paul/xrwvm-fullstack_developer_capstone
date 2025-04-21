74237eea0a1d686b2367ccc9d44d47db872a6aec
MK <105037453+MK-Paul@users.noreply.github.com>
2025-04-21 12:12:51 +0300

    added sentiment analysis

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,36 @@
-# Uncomment the following imports before adding the Model code
-
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-# Create your models here.
+# CarMake model
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)  # Name of the car make
+    description = models.TextField()  # Description of the car make
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+    def __str__(self):
+        return self.name  # String representation of the car make
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+# CarModel model
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship with CarMake
+    name = models.CharField(max_length=100)  # Name of the car model
+    dealer_id = models.IntegerField()  # Dealer ID (from Cloudant database)
+    
+    # Car type choices
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('TRUCK', 'Truck'),
+    ]
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')  # Type of car
+    year = models.IntegerField(
+        validators=[
+            MaxValueValidator(2023),  # Maximum year
+            MinValueValidator(2015)  # Minimum year
+        ]
+    )  # Year of the car model
+
+    def __str__(self):
+        return f"{self.car_make.name} {self.name}"  # String representation of the car model
